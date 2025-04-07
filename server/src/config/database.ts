7d0f6865a007cde415d5b2c9230b679e36c453bc
Thays Ribeiro <thaysmoiaribeiro@gmail.com>
2025-04-07 15:00:06 -0700

    Change log message to display 'DB_USER' instead of 'postgres'

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-console.log('postgres', process.env.DB_USER);
+console.log('DB_USER', process.env.DB_USER);
 
 export const sequelize = new Sequelize(
   process.env.DB_NAME || 'mindmotion_db', 
