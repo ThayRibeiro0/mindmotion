61290ba354b83eb1742f7a14d447c5a7ba432e5b
Thays Ribeiro <thaysmoiaribeiro@gmail.com>
2025-04-08 10:49:06 -0700

    Adds checking of essential environment variables and updates log messages in Sequelize configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,24 @@
-// src/config/database.js
+// src/config/database.ts
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
-dotenv.config(); 
+dotenv.config(); // Carrega as variáveis do arquivo .env
+
+// Verifica se as variáveis de ambiente essenciais estão definidas
+const requiredEnvVars = ['DB_HOST', 'DB_USER', 'DB_PASS', 'DB_NAME', 'DB_PORT'];
+requiredEnvVars.forEach((varName) => {
+  if (!process.env[varName]) {
+    throw new Error(`The ${varName} environment variable is not set.`);
+  }
+});
 
 const sequelize = new Sequelize({
   dialect: 'postgres',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
+  host: process.env.DB_HOST!,
+  username: process.env.DB_USER!,
+  password: process.env.DB_PASS!,
+  database: process.env.DB_NAME!,
+  port: Number(process.env.DB_PORT!),
   logging: false, // Desativa o logging, se não necessário
   dialectOptions: process.env.NODE_ENV === 'production' ? {
     ssl: {
@@ -22,7 +30,7 @@ const sequelize = new Sequelize({
 
 // Testa a conexão
 sequelize.authenticate()
-  .then(() => console.log('Conexão com o banco de dados estabelecida com sucesso!'))
-  .catch((err) => console.error('Erro ao conectar ao banco de dados:', err));
+  .then(() => console.log('Conection to database established successfully.'))
+  .catch((err) => console.error('Error connecting to the database:', err));
 
 export default sequelize;
