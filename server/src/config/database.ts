6064d4c36427dd2cc29d3dda67dd288b5f1bb73f
Thays Ribeiro <thaysmoiaribeiro@gmail.com>
2025-04-08 13:41:35 -0700

    Updates database connection settings to new environment parameters and changes the host to the production server

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -3,11 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const sequelize = new Sequelize(
-  process.env.DB_NAME || 'mindmotion_db', 
-  process.env.DB_USER || 'postgres',
-  process.env.DB_PASS || 'teste',
+  process.env.DB_NAME || 'mindmotion_db_yyzk', 
+  process.env.DB_USER || 'mindmotion_db_yyzk_user',
+  process.env.DB_PASS || 'HigiQDsGR9zLT1I7lw7yQbMXChrQgBjY',
   {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || 'dpg-cvqo35emcj7s73fctdag-a',
     port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
     dialect: 'postgres',
   }
