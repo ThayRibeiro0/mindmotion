89e9def990b32cde305244e9fad919e5129bffa8
Thays Ribeiro <thaysmoiaribeiro@gmail.com>
2025-04-08 10:44:43 -0700

    Updates Sequelize configuration to use environment variables and adds SSL support in production.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,28 @@
 // src/config/database.js
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-export const sequelize = new Sequelize({
+dotenv.config(); 
+
+const sequelize = new Sequelize({
   dialect: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASS || 'teste',
-  database: process.env.DB_NAME || 'mindmotion_db',
-  port: Number(process.env.DB_PORT) || 5432,
-  logging: false, // Disable logging if you don't need it
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
+  logging: false, // Desativa o logging, se não necessário
+  dialectOptions: process.env.NODE_ENV === 'production' ? {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  } : {}
 });
 
-// You can also handle any connection issues in this file if needed
+// Testa a conexão
 sequelize.authenticate()
-  .then(() => console.log('Database connected!'))
-  .catch((err) => console.error('Database connection error:', err));
+  .then(() => console.log('Conexão com o banco de dados estabelecida com sucesso!'))
+  .catch((err) => console.error('Erro ao conectar ao banco de dados:', err));
 
 export default sequelize;
